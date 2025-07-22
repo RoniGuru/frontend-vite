9630761f3647041c4bf76634b4cfbb227f06b524
Ronit Gurung <lecrepes45@gmail.com>
2025-07-22 15:59:22 +0100

    call logout api in logout method

--- src/components/Auth/context/AuthProvider.tsx
+++ src/components/Auth/context/AuthProvider.tsx
@@ -3,17 +3,24 @@ import type { ReactNode } from 'react';
 import type { User } from './type';
 import { AuthContext } from './authContext';
 import type { AuthContextType } from './authContext';
+import { api } from '../../../api/api';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const [accessToken, setAccessToken] = useState<string>('');
 
-  function logout() {
-    setUser(null);
-    setAccessToken('');
-    //set refresh token to empty
-    document.cookie =
-      'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  async function logout() {
+    if (user) {
+      const response = await api.post(`/logout/${user.id}`);
+
+      if (response.status === 200) {
+        setUser(null);
+        setAccessToken('');
+        //set refresh token to empty
+        document.cookie =
+          'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      }
+    }
   }
 
   const value: AuthContextType = {
