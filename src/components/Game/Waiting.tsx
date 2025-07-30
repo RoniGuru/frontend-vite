57d6c2b8fed1ed9fdccef091215c12c6a298029b
Ronit Gurung <lecrepes45@gmail.com>
2025-07-30 21:45:51 +0100

    go back button and change home layout

--- src/components/Game/Waiting.tsx
+++ src/components/Game/Waiting.tsx
@@ -1,6 +1,7 @@
 import type { AppDispatch } from '../../state/store';
 import { useDispatch } from 'react-redux';
 import { startGame } from '../../state/game/gameSlice';
+import { Link } from 'react-router-dom';
 
 const Waiting = ({
   setUserInput,
@@ -14,12 +15,20 @@ const Waiting = ({
     setUserInput('');
   }
   return (
-    <button
-      className="px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-400 transition-colors duration-200 font-semibold"
-      onClick={handleStart}
-    >
-      Start Game
-    </button>
+    <div className="flex flex-col text-center">
+      <button
+        className="px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-400 transition-colors duration-200 font-semibold"
+        onClick={handleStart}
+      >
+        Start Game
+      </button>
+      <Link
+        to="/"
+        className="px-6 py-3 mt-4 bg-gray-600 text-white rounded-lg hover:bg-gray-400 transition-colors duration-200 font-semibold"
+      >
+        Go Back
+      </Link>
+    </div>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,20 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center  gap-6 justify-center h-full">
-      <div className="text-white text-4xl font-bold ">Welcome {user?.name}</div>
-      <LeaderBoard />
-      <Link
-        to="/game"
-        className="p-4 bg-slate-400 w-36 text-center font-bold rounded-lg hover:bg-slate-700 "
-      >
-        Play Game
-      </Link>
+      <div className="flex  w-3/4 items-center justify-between ">
+        <LeaderBoard />
+        <div className="h-full flex flex-col items-center gap-10  w-full pr-36">
+          <div className="text-white text-4xl font-bold ">
+            Welcome {user?.name}
+          </div>
+          <Link
+            to="/game"
+            className="p-4 bg-slate-400 w-36 text-center font-bold rounded-lg hover:bg-slate-700 "
+          >
+            Play Game
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
