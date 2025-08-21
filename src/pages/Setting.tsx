acbadfd5866bf78c0c8936d18fae076ff58ebd7a
Ronit Gurung <lecrepes45@gmail.com>
2025-08-21 13:16:17 +0100

    setting back button and header change

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,9 +1,12 @@
 import UserForm from '../components/User/UserForm';
 import { useState } from 'react';
 import { useAuth } from '../Context/Auth/useAuth';
+import { useNavigate } from 'react-router-dom';
+
 const Setting = () => {
   const [option, setOption] = useState(0);
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   function renderOption() {
     switch (option) {
@@ -18,13 +21,25 @@ const Setting = () => {
       <div className="h-[90%] w-[90%] bg-slate-300 p-10 flex flex-row">
         {/* Options */}
         <div className="w-[20%]  p-2">
+          <button
+            className="text-black hover:text-white text-sm transition-colors"
+            onClick={() => navigate(-1)}
+          >
+            Back
+          </button>
           <h1 className="text-4xl font-bold mb-4 ">Setting</h1>
-          <h1 className="text-2xl font-bold" onClick={() => setOption(1)}>
+          <h1
+            className="text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg"
+            onClick={() => setOption(1)}
+          >
             User
           </h1>
-          <button className="text-2xl font-bold" onClick={logout}>
+          <h1
+            className="text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg"
+            onClick={logout}
+          >
             Logout
-          </button>
+          </h1>
         </div>
         {/* Options */}
         <div className="bg-slate-400 w-[80%] flex justify-center p-4 rounded-xl">
