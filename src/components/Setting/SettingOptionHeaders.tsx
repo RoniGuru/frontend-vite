b4ffb3faab8f269476fd49bf1814a2048e73da40
Ronit Gurung <lecrepes45@gmail.com>
2025-09-09 12:16:59 +0100

    Setting Options Header

---
+++ src/components/Setting/SettingOptionHeaders.tsx
@@ -0,0 +1,43 @@
+import { useAuth } from '../../Context/Auth/useAuth';
+import { useNavigate } from 'react-router-dom';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+
+const SettingOptionHeaders = ({
+  option,
+  setOption,
+}: {
+  option: number;
+  setOption: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  return (
+    <div className="w-[20%]   ">
+      <h1 className="text-4xl font-bold mb-4  flex flex-row ">
+        <button
+          className="text-black hover:text-white font-bold text-2xl transition-colors flex flex-row text-center items-center mr-2"
+          onClick={() => navigate(-1)}
+        >
+          <IoMdArrowRoundBack size={40} />
+        </button>
+        Setting
+      </h1>
+      <h1
+        className={`text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg pl-12 w-[95%] ${
+          option == 1 ? '' : 'bg-slate-500 text-white'
+        }`}
+        onClick={() => setOption(1)}
+      >
+        Profile
+      </h1>
+      <h1
+        className="text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg  pl-12 w-[95%]"
+        onClick={logout}
+      >
+        Logout
+      </h1>
+    </div>
+  );
+};
+
+export default SettingOptionHeaders;
--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,13 +1,9 @@
 import UserSetting from '../components/User/UserSetting';
 import { useState } from 'react';
-import { useAuth } from '../Context/Auth/useAuth';
-import { useNavigate } from 'react-router-dom';
-import { IoMdArrowRoundBack } from 'react-icons/io';
+import SettingOptionHeaders from '../components/Setting/SettingOptionHeaders';
 
 const Setting = () => {
   const [option, setOption] = useState(0);
-  const { logout } = useAuth();
-  const navigate = useNavigate();
 
   function renderOption() {
     switch (option) {
@@ -22,29 +18,7 @@ const Setting = () => {
       <div className="h-[90%] w-[90%] bg-slate-300 p-10 flex flex-row">
         {/* Options */}
 
-        <div className="w-[20%]  ">
-          <h1 className="text-4xl font-bold mb-4  flex flex-row ">
-            <button
-              className="text-black hover:text-white font-bold text-2xl transition-colors flex flex-row text-center items-center mr-2"
-              onClick={() => navigate(-1)}
-            >
-              <IoMdArrowRoundBack size={40} />
-            </button>
-            Setting
-          </h1>
-          <h1
-            className="text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg pl-12"
-            onClick={() => setOption(1)}
-          >
-            Profile
-          </h1>
-          <h1
-            className="text-2xl font-bold cursor-pointer hover:bg-slate-400 p-2 duration-100 ease-in-out rounded-lg pl-12"
-            onClick={logout}
-          >
-            Logout
-          </h1>
-        </div>
+        <SettingOptionHeaders option={option} setOption={setOption} />
         {/* Options */}
         <div className="bg-slate-400 w-[80%] flex  p-4 rounded-xl ">
           {renderOption()}
